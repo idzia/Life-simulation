a17fa9e4a8f1684e4ce7c4fa08e8578e7e3f7812
piotrkaminski95 <pkaminki95@gmail.com>
2019-01-21 21:20:53 +0100

    setup BoardController

--- src/main/java/com/codecool/controller/BoardController.java
+++ src/main/java/com/codecool/controller/BoardController.java
@@ -1,4 +1,31 @@
 package com.codecool.controller;
 
+import com.codecool.model.Position;
+import com.codecool.model.board.Board;
+
+import javax.print.attribute.standard.Destination;
+
 public class BoardController {
+
+    public boolean moveCreature(Board board, Position pos, Destination dest) { return false; }
+
+    private boolean isMoveValid(Board board, Position pos, Destination dest) { return false;}
+
+    private void makeMove(Position pos, Destination dest) {}
+
+    private void moveNorth(Position pos) {}
+
+    private void moveNorthEast(Position pos) {}
+
+    private void moveNorthWest(Position pos) {}
+
+    private void moveSouth(Position pos) {}
+
+    private void moveSouthEast(Position pos) {}
+
+    private void moveSouthWest(Position pos) {}
+
+    private void moveEast(Position pos) {}
+
+    private void moveWest(Position pos) {}
 }
