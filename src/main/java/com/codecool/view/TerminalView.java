5d127bb542a748c623cb5484c2557952b5bffd86
kret0 <mjp.sik@gmail.com>
2019-01-21 20:24:04 +0100

    Implement terminal view

--- src/main/java/com/codecool/view/TerminalView.java
+++ src/main/java/com/codecool/view/TerminalView.java
@@ -1,9 +1,33 @@
 package com.codecool.view;
 
 import com.codecool.model.board.Board;
+import com.codecool.model.board.Cell;
 
 public class TerminalView implements View{
     public void draw(Board board) {
 
+        StringBuilder sb = new StringBuilder();
+        for (Cell[] row: board.getBoard()) {
+            sb.append("\n");
+            for(Cell cell: row) {
+                if(cell.getCurrentCreature() != null) {
+                    sb.append("C ");
+                } else
+                    if(cell.getFoodAmmount() > 0){
+                    sb.append("F ");
+                    } else{
+                    sb.append("_ ");
+                    }
+            }
+
+        }
+        clearScreen();
+        System.out.println(sb.toString());
+
+    }
+
+    private void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 }
